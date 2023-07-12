cf786c4724d8868ca14c32eed2b9cb098df6220b
Erick Estrada <erickestradapj@gmail.com>
2023-07-11 23:22:32 -0500

    feat: custom hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { HooksApp } from './HooksApp.jsx';
-import { CounterApp } from './01-useState/CounterApp';
+import { CounterWithCustomHook } from './CounterWithCustomHook';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp />
+    <CounterWithCustomHook />
   </React.StrictMode>,
 );
