55bbfddd9be22d8b7e7ad74d67c7354e25a1ac2a
Erick Estrada <erickestradapj@gmail.com>
2023-07-13 20:13:50 -0500

    feat: useFetch + useCounter

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -5,12 +5,21 @@ export const MultipleCustomHooks = () => {
     'https://rickandmortyapi.com/api/character/1',
   );
 
-  console.log({ data, isLoading, hasError });
-
   return (
     <>
       <h1>Breaking Quotes</h1>
       <hr />
+
+      {isLoading ? (
+        <div className='alert alert-info text-center'>Loading...</div>
+      ) : (
+        <blockquote className='blockquote text-end'>
+          <p className='mb-1'>{data.name}</p>
+          <footer className='blockquote-footer'>{data.species}</footer>
+        </blockquote>
+      )}
+
+      <button className='btn btn-primary'>Next quote</button>
     </>
   );
 };
