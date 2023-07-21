c5ce8beed0476f3dc9ebc97d4ed8e73eb987f743
Erick Estrada <erickestradapj@gmail.com>
2023-07-20 23:21:59 -0500

    feat: delete TODO

--- src/07-useReducer/TodoApp.jsx
+++ src/07-useReducer/TodoApp.jsx
@@ -31,6 +31,14 @@ export const TodoApp = () => {
     dispatch(action);
   };
 
+  const handleDeleteTodo = (id) => {
+    console.log(id);
+    dispatch({
+      type: '[TODO] Remove Todo',
+      payload: id,
+    });
+  };
+
   return (
     <>
       <h1>
@@ -40,7 +48,7 @@ export const TodoApp = () => {
 
       <div className='row'>
         <div className='col-7'>
-          <TodoList todos={todos} />
+          <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
         </div>
 
         <div className='col-5'>
