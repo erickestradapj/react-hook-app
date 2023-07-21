ae26b5394275cebdf0f14c0f085d91cf95d709f2
Erick Estrada <erickestradapj@gmail.com>
2023-07-20 23:51:23 -0500

    feat: Toggle Todo

--- src/07-useReducer/TodoApp.jsx
+++ src/07-useReducer/TodoApp.jsx
@@ -39,6 +39,14 @@ export const TodoApp = () => {
     });
   };
 
+  const handleToggleTodo = (id) => {
+    console.log(id);
+    dispatch({
+      type: '[TODO] Toggle Todo',
+      payload: id,
+    });
+  };
+
   return (
     <>
       <h1>
@@ -48,7 +56,11 @@ export const TodoApp = () => {
 
       <div className='row'>
         <div className='col-7'>
-          <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
+          <TodoList
+            todos={todos}
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={handleToggleTodo}
+          />
         </div>
 
         <div className='col-5'>
