7d51c8722b6916055ecc47f9779eb7dacec79f94
Erick Estrada <erickestradapj@gmail.com>
2023-07-21 20:55:55 -0500

    feat: NavLink

--- src/08-useContext/Navbar.jsx
+++ src/08-useContext/Navbar.jsx
@@ -1,11 +1,48 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
-    <>
-      <Link to=''>Home</Link>
-      <Link to='/about'>About</Link>
-      <Link to='/login'>Login</Link>
-    </>
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark rounded-3'>
+      <div className='container-fluid'>
+        <Link className='navbar-brand' to='/'>
+          useContext
+        </Link>
+
+        <div className='collapse navbar-collapse' id='navbarNav'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <NavLink
+                className={({ isActive }) =>
+                  `nav-link ${isActive ? 'active' : ''}`
+                }
+                to='/'
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink
+                className={({ isActive }) =>
+                  `nav-link ${isActive ? 'active' : ''}`
+                }
+                to='/about'
+              >
+                About
+              </NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink
+                className={({ isActive }) =>
+                  `nav-link ${isActive ? 'active' : ''}`
+                }
+                to='/login'
+              >
+                Login
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
